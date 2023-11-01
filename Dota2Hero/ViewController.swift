6b2a1bb32a47dfdf835e92787dc1e1c758f97d73
Vosik <vosikvos@gmail.com>
2023-11-01 22:16:29 +0200

    Initial Commit

---
+++ Dota2Hero/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dota2Hero
+//
+//  Created by Саша Восколович on 01.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
