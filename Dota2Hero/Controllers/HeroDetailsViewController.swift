891c9c3db5b6e3cd402b0131c9efdc696ea0f79c
Vosik <vosikvos@gmail.com>
2023-11-09 23:36:03 +0200

    added new controller

---
+++ Dota2Hero/Controllers/HeroDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HeroDetailsViewController.swift
+//  Dota2Hero
+//
+//  Created by Саша Восколович on 09.11.2023.
+//
+
+import UIKit
+
+class HeroDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Dota2Hero/Supporting/LabelFactory.swift
@@ -0,0 +1,8 @@
+//
+//  LabelFactory.swift
+//  Dota2Hero
+//
+//  Created by Саша Восколович on 09.11.2023.
+//
+
+import Foundation
